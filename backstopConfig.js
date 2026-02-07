9c0f7236c8bba34362d86c1f2fbbfba93c32b5a5
Johann Patrício Daniel <johannpatriciod@gmail.com>
2026-02-06 22:16:22 -0300

    subindo atividade

--- backstopConfig.js
+++ backstopConfig.js
@@ -3,23 +3,34 @@
 // https://github.com/garris/BackstopJS#advanced-scenarios
 
 const backstop = require('@mate-academy/backstop-config');
-
 const { basicScenario } = backstop;
+
+const PAGE_URL =
+  'https://johannpdaniel.github.io/layout_hello-world/';
 
 const config = {
   ...backstop,
+
   fileNameTemplate: '{scenarioLabel}',
-  viewports: [{
-    name: 'custom-size',
-    width: 200,
-    height: 50,
-  }],
+
+  viewports: [
+    {
+      name: 'custom-size',
+      width: 200,
+      height: 50,
+    },
+  ],
+
   scenarios: [
     {
       ...basicScenario,
       label: 'Entire document',
-      referenceUrl: basicScenario.referenceUrl + '/hello-world/',
-    }],
+      url: PAGE_URL,
+      referenceUrl: PAGE_URL,
+      testUrl: PAGE_URL,
+      selectors: ['document'],
+    },
+  ],
 };
 
 module.exports = config;
